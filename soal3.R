2f60af01ea565f61f6ac999f3789e3ad9c42b2ee
eldrus <103352161+eldrus@users.noreply.github.com>
2022-04-10 16:10:39 +0700

    add soal 3

---
+++ soal3.R
@@ -0,0 +1,17 @@
+#soal 3
+#a
+lambda=4.5
+x=6
+probability = dpois(x, lambda)
+probability
+
+#b
+x=6
+n=365
+hist(rpois(n,lambda),main= "histogram poissson ")
+
+#c
+#d
+mean=variance=lamda
+mean
+variance
