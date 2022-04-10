6004403d69a23736b57cc4249ba60b1a99d9b5c4
eldrus <103352161+eldrus@users.noreply.github.com>
2022-04-10 21:47:56 +0700

    update soal5

--- soal5.R
+++ soal5.R
@@ -2,3 +2,10 @@
 set.seed(1)
 probability = dexp(10, rate = lambda)
 probability
+
+#b
+set.seed(1)
+hist(rexp(10, rate = lambda), main = "Histogram Exponetial  10 ")
+hist(rexp(100, rate = lambda), main = "Histogram Exponetial  100 ")
+hist(rexp(1000, rate = lambda), main = "Histogram Exponetial  1000 ")
+hist(rexp(10000, rate = lambda), main = "Histogram Exponetial  10000")
