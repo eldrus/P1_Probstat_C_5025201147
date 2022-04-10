9bbb64711a8a2a22f32302231ea83935b33cca7d
eldrus <103352161+eldrus@users.noreply.github.com>
2022-04-10 13:38:49 +0700

    add soal1

---
+++ soal1.R
@@ -0,0 +1,25 @@
+#soal 1
+# a
+dgeom(1,0.20)+dgeom(2,0.20)+dgeom(3,0.20)
+
+#b
+p = 0.20
+x = 3
+mean(rgeom(n=10000, prob = p) ==3)
+mean
+
+#c
+#d
+n=10000
+hist(rgeom(n, prob = p), main = 'Histogram Geometrik')
+
+#e
+mean = 1 / p
+variance = (1 - p) / (p ^ 2)
+mean
+variance 
+
+
+
+
+
