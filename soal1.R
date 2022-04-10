9d9416ef2edc8a11ea4194459130846c89ee9345
eldrus <103352161+eldrus@users.noreply.github.com>
2022-04-10 16:17:03 +0700

    update soal 1

--- soal1.R
+++ soal1.R
@@ -5,13 +5,13 @@ dgeom(1,0.20)+dgeom(2,0.20)+dgeom(3,0.20)
 #b
 p = 0.20
 x = 3
-mean(rgeom(n=10000, prob = p) ==3)
+mean(rgeom(n=10000,  p) ==3)
 mean
 
 #c
 #d
 n=10000
-hist(rgeom(n, prob = p), main = 'Histogram Geometrik')
+hist(rgeom(n, p), main = 'Histogram Geometrik')
 
 #e
 mean = 1 / p
