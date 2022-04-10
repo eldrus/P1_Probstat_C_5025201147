39003ab32b2e30f3a83e2659f94882166ea65257
eldrus <103352161+eldrus@users.noreply.github.com>
2022-04-10 16:35:27 +0700

    Add files via upload

---
+++ soal4.R
@@ -0,0 +1,19 @@
+#soal 4
+x=2
+v=10
+#a
+
+probability = dchisq(x, 10)
+probability
+
+
+
+#b
+n=100
+hist(rchisq(n, v), xlab = "X", ylab = "V", main = " Histogram chi-square")
+
+#c
+mean=v
+variance= 2 * v
+mean
+variance
